c2ed5fe600fc647f8f03319dbfe9a05aee84eea9
Denis Tomarev <dentomarev@gmail.com>
2024-02-17 12:19:10 +0300

    [3] Реализовал движение игррока

--- Assets/Scripts/Movement/MovementController.cs
+++ Assets/Scripts/Movement/MovementController.cs
@@ -4,22 +4,34 @@ using UnityEngine;
 
 public class MovementController : MonoBehaviour, IMovable
 {
-    public float MovementSpeed => throw new System.NotImplementedException();
+    public float MovementSpeed
+    {
+        get { return _movementSpeed; }
+    }
+    [SerializeField] private float _movementSpeed;
 
+    /// <summary>
+    /// Перемещает объект в указанном направлении с учетом скорости движения.
+    /// </summary>
+    /// <param name="direction">Направление движения в виде вектора Vector3.</param>
     public void Move(Vector3 direction)
     {
-        throw new System.NotImplementedException();
+        if (float.IsNaN(_movementSpeed))
+        {
+            return;
+        }
+
+        if (_movementSpeed < 0)
+        {
+            _movementSpeed = 0;
+        }
+
+        gameObject.GetComponent<Rigidbody>().AddForce(direction * _movementSpeed, ForceMode.VelocityChange);
     }
 
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        _movementSpeed = 0;
+        gameObject.GetComponent<Rigidbody>().freezeRotation = true;
     }
 }
