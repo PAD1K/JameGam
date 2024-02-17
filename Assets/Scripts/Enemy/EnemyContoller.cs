9ce224af0240aca089b4385f511f07c7a19793eb
Aramymo <maxoff9617@gmail.com>
2024-02-17 15:34:51 +0300

    [5] Реализовал методы IDamagable в EnemyController

--- Assets/Scripts/Enemy/EnemyContoller.cs
+++ Assets/Scripts/Enemy/EnemyContoller.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyContoller : MonoBehaviour, IShootable, IDamageable
+public class EnemyContoller : MonoBehaviour, IDamageable
 {
     [SerializeField] private float _moveTime;
     [SerializeField] private float _attackTime;
@@ -36,18 +36,25 @@ public class EnemyContoller : MonoBehaviour, IShootable, IDamageable
         
     }
 
-    public void Shoot(Vector3 direction)
-    {
-
-    }
-
     public void Damage(int damage)
     {
-
+        if(_health < 0)
+        {
+            return;
+        }
+        if(damage < 0)
+        {
+            damage = 0;
+        }
+        _health -= damage;
+        if(_health <= 0)
+        {
+            Death();
+        }
     }
 
     public void Death()
     {
-
+        gameObject.SetActive(false);
     }
 }
