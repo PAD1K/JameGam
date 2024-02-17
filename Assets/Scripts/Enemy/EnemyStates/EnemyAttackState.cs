9f0674b75160887593d8de82026147b1eedd1a4b
Aramymo <maxoff9617@gmail.com>
2024-02-17 15:34:51 +0300

    [5] Реализовал методы IDamagable в EnemyController

--- Assets/Scripts/Enemy/EnemyStates/EnemyAttackState.cs
+++ Assets/Scripts/Enemy/EnemyStates/EnemyAttackState.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyAttackState : StateMachineBehaviour
+public class EnemyAttackState : StateMachineBehaviour, IShootable
 {
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         Debug.Log("Attacked");
+        //Shoot();
         animator.SetBool("IsAttacking", false);
     }
 
@@ -19,6 +20,11 @@ public class EnemyAttackState : StateMachineBehaviour
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+    }
+
+    public void Shoot(Vector3 direction)
     {
         
     }
