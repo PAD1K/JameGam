5c7e0de3e6ad9ca566681e94527d8e2925d58fec
Aramymo <maxoff9617@gmail.com>
2024-02-17 15:42:05 +0300

    [5] Добавил метод Shoot в Attack State

--- Assets/Scripts/Enemy/EnemyStates/EnemyAttackState.cs
+++ Assets/Scripts/Enemy/EnemyStates/EnemyAttackState.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class EnemyAttackState : StateMachineBehaviour, IShootable
 {
+    [SerializeField] private IShootable _shootController;
+    [SerializeField] private Transform _playerTransform;
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         Debug.Log("Attacked");
-        //Shoot();
+        _shootController.Shoot(_playerTransform.position);
         animator.SetBool("IsAttacking", false);
     }
 
