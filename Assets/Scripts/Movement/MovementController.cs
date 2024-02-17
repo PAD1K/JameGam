1a33f71354655c75c2859bbc84aeeaf8aea1b519
Denis Tomarev <dentomarev@gmail.com>
2024-02-17 16:48:51 +0300

    [24] Изменил 2д на 3д

--- Assets/Scripts/Movement/MovementController.cs
+++ Assets/Scripts/Movement/MovementController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Rigidbody))]
 public class MovementController : MonoBehaviour, IMovable
 {
     public float MovementSpeed
