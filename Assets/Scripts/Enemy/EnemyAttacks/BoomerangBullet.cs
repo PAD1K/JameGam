c5ce7f908e7ba06b4310f6d0baebcc32fb59cb7a
Aramymo <maxoff9617@gmail.com>
2024-02-19 00:40:24 +0300

    afafafaf

--- Assets/Scripts/Enemy/EnemyAttacks/BoomerangBullet.cs
+++ Assets/Scripts/Enemy/EnemyAttacks/BoomerangBullet.cs
@@ -29,7 +29,7 @@ public class BoomerangBullet : MonoBehaviour
 
     IEnumerator TurnBack()
     {
-        yield return new WaitForSeconds(4f);
+        yield return new WaitForSeconds(2.5f);
         Debug.Log("Aboba");
         Rigidbody.velocity = -Rigidbody.velocity;
     }
